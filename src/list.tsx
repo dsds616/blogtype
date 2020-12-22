42220006cc3e02a8839a16a1af9572c350409dac
dskim <dskim@11h11m.com>
2020-12-22 12:16:46 +0900

    a

--- src/list.tsx
+++ src/list.tsx
@@ -9,7 +9,7 @@ function List(){
                 <div className="media-body">
                     <h3 className="title mb-1"><a href="blog-post.html">Why Every Developer Should Have A Blog</a></h3>
                     <div className="meta mb-1"><span className="date">Published 2 days ago</span><span className="time">5 min read</span><span className="comment"><a href="#">8 comments</a></span></div>
-                    <div className="intro">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies...</div>
+                    <div className="intro">ㅇㅇㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ</div>
                     <a className="more-link" href="blog-post.html">Read more &rarr;</a>
                 </div>
             </div>
