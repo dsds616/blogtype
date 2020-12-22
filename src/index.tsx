7c8a3db60eeba60cb9cfa587712650211bfcf6a5
dskim <dskim@11h11m.com>
2020-12-22 12:06:18 +0900

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import List from './list';
 import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('tst')
+// );
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <List />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('blogList')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
